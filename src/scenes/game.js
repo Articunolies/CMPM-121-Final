4b9081b2c60e52d701bf5d9d1d45f913f3acc01a
Justin-Lam <justinflam9225k@gmail.com>
2024-11-20 20:44:37 -0800

    Created a player with 8 direction movement

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,16 +1,67 @@
-class Game extends Phaser.Scene
-{
+class Game extends Phaser.Scene {
+
+	PLAYER_VELOCITY = 100;
+
 	constructor() {
 		super("gameScene");
 	}
 
-	create()
-	{
+	create() {
+
+		this.setInput();
 		
+		// Create player
+		this.player = this.physics.add.sprite(100, 100, "player");
+		this.player.setCollideWorldBounds(true);
 	}
 
-    update()
-    {
+    update() {
 
+		this.handlePlayerMovement();
     }
+
+	setInput() {
+		this.moveUpKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+		this.moveDownKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+		this.moveLeftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+		this.moveRightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+	}
+
+	handlePlayerMovement() {
+		
+		// For making sure the player doesn't move faster diagonally
+		let numMoveDirections = 0;
+
+		// Vertical Movement
+		if (this.moveUpKey.isDown) {
+			this.player.body.setVelocityY(-this.PLAYER_VELOCITY);
+			numMoveDirections++;
+		}
+		else if (this.moveDownKey.isDown) {
+			this.player.body.setVelocityY(this.PLAYER_VELOCITY);
+			numMoveDirections++;
+		}
+		else {
+			this.player.body.setVelocityY(0);
+		}
+
+		// Horizontal Movement
+		if (this.moveLeftKey.isDown) {
+			this.player.body.setVelocityX(-this.PLAYER_VELOCITY);
+			numMoveDirections++;
+		}
+		else if (this.moveRightKey.isDown) {
+			this.player.body.setVelocityX(this.PLAYER_VELOCITY);
+			numMoveDirections++;
+		}
+		else {
+			this.player.body.setVelocityX(0);
+		}
+
+		// Make sure the player doesn't move faster diagonally
+		if (numMoveDirections > 1) {
+			this.player.body.velocity.x /= Math.SQRT2;
+			this.player.body.velocity.y /= Math.SQRT2;
+		}
+	}
 }
--- src/scenes/load.js
+++ src/scenes/load.js
@@ -1,16 +1,23 @@
-class Load extends Phaser.Scene
-{
+class Load extends Phaser.Scene {
+	
 	constructor() {
 		super("loadScene");
 	}
 
-	preload()
-	{
+	preload() {
+
 		this.load.path = './assets/';
+		this.load.image("dirt", "dirt.png");
+		this.load.image("grass1", "grass1.png");
+		this.load.image("grass2", "grass2.png");
+		this.load.image("mushroom1", "mushroom1.png");
+		this.load.image("mushroom2", "mushroom2.png");
+		this.load.image("player", "player1.png");
+
 	}
 
-	create()
-	{
+	create() {
+
 		this.scene.start("gameScene");
 	}
 }
